a110c84aff6a7c7c1b245ad5380e54542d66bb6b
freedomserg <freedomserg97@gmail.com>
2016-11-07 16:01:51 +0200

    minor changes in project structure

--- restaurant-core/src/main/java/net/freedomserg/restaurant/core/main/Bootstrap.java
+++ restaurant-core/src/main/java/net/freedomserg/restaurant/core/main/Bootstrap.java
@@ -1,8 +1,29 @@
 package net.freedomserg.restaurant.core.main;
 
+import net.freedomserg.restaurant.core.service.EmployeeService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Bootstrap {
 
+    private EmployeeService employeeService;
+
+    public void setEmployeeService(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
     public static void main(String[] args) {
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-context.xml",
+                "hibernate-context.xml");
+        Bootstrap bootstrap = applicationContext.getBean("bootstrap", Bootstrap.class);
+        bootstrap.start();
+
+    }
+
+    private void start() {
+        //hEmployeeController.printEmployees();
+        employeeService.getAll().forEach(System.out::println);
 
     }
 }
