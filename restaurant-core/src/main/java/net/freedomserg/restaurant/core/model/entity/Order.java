4f5ec9305ca6e4244be12f93f685ba1b7a0c8310
freedomserg <freedomserg97@gmail.com>
2016-11-09 19:47:10 +0200

    core: prepared Order and Waiter entities

--- restaurant-core/src/main/java/net/freedomserg/restaurant/core/model/entity/Order.java
+++ restaurant-core/src/main/java/net/freedomserg/restaurant/core/model/entity/Order.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "ordering")
@@ -21,10 +23,71 @@ public class Order {
 
     @ManyToOne
     @JoinColumn(name = "waiter_id")
-    private Employee waiter;
+    private Waiter waiter;
 
     @Column(name = "table_number")
     private int tableNumber;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "dish_to_order",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "dish_id")
+    )
+    private List<Dish> dishes;
 
+    @Column(name = "order_date")
+    private Date orderDate;
+
+    @Column(name = "order_state")
+    @Enumerated(EnumType.STRING)
+    private OrderState state;
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public Waiter getWaiter() {
+        return waiter;
+    }
+
+    public void setWaiter(Waiter waiter) {
+        this.waiter = waiter;
+    }
+
+    public int getTableNumber() {
+        return tableNumber;
+    }
+
+    public void setTableNumber(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public List<Dish> getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(List<Dish> dishes) {
+        this.dishes = dishes;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderState getState() {
+        return state;
+    }
+
+    public void setState(OrderState state) {
+        this.state = state;
+    }
 }
---
+++ restaurant-core/src/main/java/net/freedomserg/restaurant/core/model/entity/Waiter.java
@@ -0,0 +1,42 @@
+package net.freedomserg.restaurant.core.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import net.freedomserg.restaurant.core.service.EmployeeService;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+
+@Entity
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id")
+public class Waiter extends Employee {
+
+    @OneToMany(mappedBy = "waiter", fetch = FetchType.EAGER)
+    @Fetch(FetchMode.SELECT)
+    private List<Order> orders;
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(super.toString());
+        builder.append("orders: ").append("\n");
+        orders.forEach(order -> builder.append(order).append("\n"));
+        return builder.toString();
+    }
+
+}
