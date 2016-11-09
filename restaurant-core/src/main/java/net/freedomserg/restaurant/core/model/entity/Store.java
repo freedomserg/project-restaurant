244bb6dd92e311bad89448e7bc66cac3185e8b85
freedomserg <freedomserg97@gmail.com>
2016-11-09 20:43:32 +0200

    core: prepare Store entity

---
+++ restaurant-core/src/main/java/net/freedomserg/restaurant/core/model/entity/Store.java
@@ -0,0 +1,35 @@
+package net.freedomserg.restaurant.core.model.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "store")
+public class Store {
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    @Column(name = "ingredient_id")
+    private int ingredientId;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    public int getIngredientId() {
+        return ingredientId;
+    }
+
+    public void setIngredientId(int ingredientId) {
+        this.ingredientId = ingredientId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
