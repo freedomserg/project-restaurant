233b04526230068c4e75708ffd86abaa0175ca9b
freedomserg <freedomserg97@gmail.com>
2016-11-09 19:48:57 +0200

    core: override equals(), hashCode(), toString() in Order

--- restaurant-core/src/main/java/net/freedomserg/restaurant/core/model/entity/Order.java
+++ restaurant-core/src/main/java/net/freedomserg/restaurant/core/model/entity/Order.java
@@ -90,4 +90,43 @@ public class Order {
     public void setState(OrderState state) {
         this.state = state;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+
+        if (orderId != order.orderId) return false;
+        if (tableNumber != order.tableNumber) return false;
+        if (waiter != null ? !waiter.equals(order.waiter) : order.waiter != null) return false;
+        if (dishes != null ? !dishes.equals(order.dishes) : order.dishes != null) return false;
+        if (orderDate != null ? !orderDate.equals(order.orderDate) : order.orderDate != null) return false;
+        return state == order.state;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = orderId;
+        result = 31 * result + (waiter != null ? waiter.hashCode() : 0);
+        result = 31 * result + tableNumber;
+        result = 31 * result + (dishes != null ? dishes.hashCode() : 0);
+        result = 31 * result + (orderDate != null ? orderDate.hashCode() : 0);
+        result = 31 * result + (state != null ? state.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", waiter=" + waiter +
+                ", tableNumber=" + tableNumber +
+                ", dishes=" + dishes +
+                ", orderDate=" + orderDate +
+                ", state=" + state +
+                '}';
+    }
 }
