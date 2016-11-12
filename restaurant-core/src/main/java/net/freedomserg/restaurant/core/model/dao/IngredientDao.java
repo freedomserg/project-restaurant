e129a3b655cdc5a1b9e92e7e43e07aeef144d2d5
freedomserg <freedomserg97@gmail.com>
2016-11-12 17:00:01 +0200

    core: prepare IngredientDao

---
+++ restaurant-core/src/main/java/net/freedomserg/restaurant/core/model/dao/IngredientDao.java
@@ -0,0 +1,18 @@
+package net.freedomserg.restaurant.core.model.dao;
+
+import net.freedomserg.restaurant.core.model.entity.Ingredient;
+
+import java.util.List;
+
+public interface IngredientDao {
+
+    void save(Ingredient ingredient);
+
+    void remove(Ingredient ingredient);
+
+    void update(Ingredient ingredient);
+
+    Ingredient loadByName(String name);
+
+    List<Ingredient> findAll();
+}
