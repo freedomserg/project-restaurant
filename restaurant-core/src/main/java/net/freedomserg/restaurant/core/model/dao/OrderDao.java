130a0f9819255a4e7fe303427aee4d797430038a
freedomserg <freedomserg97@gmail.com>
2016-11-12 17:42:15 +0200

    core: prepare OrderDao

---
+++ restaurant-core/src/main/java/net/freedomserg/restaurant/core/model/dao/OrderDao.java
@@ -0,0 +1,25 @@
+package net.freedomserg.restaurant.core.model.dao;
+
+import net.freedomserg.restaurant.core.model.entity.Order;
+import net.freedomserg.restaurant.core.model.entity.Waiter;
+
+import java.util.List;
+
+public interface OrderDao {
+
+    void save(Order order);
+
+    void remove(Order order);
+
+    void update(Order order);
+
+    Order loadById(int id);
+
+    List<Order> loadAll();
+
+    List<Order> loadByDate(String date);
+
+    List<Order> loadByWaiter(Waiter waiter);
+
+    List<Order> loadByTable(int tableNumber);
+}
