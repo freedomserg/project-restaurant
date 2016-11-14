9b39ae8c0af65952296d3d7e6b97370eaf146b14
freedomserg <freedomserg97@gmail.com>
2016-11-14 21:36:53 +0200

    core: rename exception

---
+++ restaurant-core/src/main/java/net/freedomserg/restaurant/core/model/exception/IllegalOperationRestaurantException.java
@@ -0,0 +1,8 @@
+package net.freedomserg.restaurant.core.model.exception;
+
+public class IllegalOperationRestaurantException extends RestaurantException {
+
+    public IllegalOperationRestaurantException(String message) {
+        super(message);
+    }
+}
