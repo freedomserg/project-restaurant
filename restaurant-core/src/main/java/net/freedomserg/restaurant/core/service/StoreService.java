c95cd133ce6cb3550c3c5cd7fddcdcc22ebbfef8
freedomserg <freedomserg97@gmail.com>
2016-11-17 20:44:39 +0200

    core: prepared StoreService

---
+++ restaurant-core/src/main/java/net/freedomserg/restaurant/core/service/StoreService.java
@@ -0,0 +1,41 @@
+package net.freedomserg.restaurant.core.service;
+
+import net.freedomserg.restaurant.core.model.dao.StoreDao;
+import net.freedomserg.restaurant.core.model.entity.Store;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public class StoreService {
+
+    private StoreDao storeDao;
+
+    public void setStoreDao(StoreDao storeDao) {
+        this.storeDao = storeDao;
+    }
+
+    @Transactional
+    public void add(Store store) {
+        storeDao.save(store);
+    }
+
+    @Transactional
+    public void delete(Store store) {
+        storeDao.remove(store);
+    }
+
+    @Transactional
+    public void update(Store store) {
+        storeDao.update(store);
+    }
+
+    @Transactional
+    public Store getByName(String name) {
+        return storeDao.loadByName(name);
+    }
+
+    @Transactional
+    public List<Store> getAll() {
+        return storeDao.loadAll();
+    }
+}
