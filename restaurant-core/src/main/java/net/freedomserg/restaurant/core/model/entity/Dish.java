20b7661691e01de425d22a1d77da2fe56bb775f5
freedomserg <freedomserg97@gmail.com>
2016-11-24 00:07:12 +0200

    core: cosmetic fix in Dish entity

--- restaurant-core/src/main/java/net/freedomserg/restaurant/core/model/entity/Dish.java
+++ restaurant-core/src/main/java/net/freedomserg/restaurant/core/model/entity/Dish.java
@@ -140,7 +140,7 @@ public class Dish implements Serializable {
         builder.append(", dishName=").append(dishName);
         builder.append(", category=").append(category);
         builder.append(", units:").append("\n");
-        units.forEach(ingredient -> builder.append(ingredient).append("\n"));
+        units.forEach(unit -> builder.append(unit).append("\n"));
         builder.append(", price=").append(price);
         builder.append(", weight=").append(weight);
         builder.append(", status=").append(status);
