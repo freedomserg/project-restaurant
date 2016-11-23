3cf8109a23aa81144ec943e4110768aa04b1b795
freedomserg <freedomserg97@gmail.com>
2016-11-23 12:53:35 +0200

    core: created SuchEntityAlreadyExustsException

---
+++ restaurant-core/src/main/java/net/freedomserg/restaurant/core/model/exception/SuchEntityAlreadyExistsRestaurantException.java
@@ -0,0 +1,8 @@
+package net.freedomserg.restaurant.core.model.exception;
+
+public class SuchEntityAlreadyExistsRestaurantException extends RestaurantException {
+
+    public SuchEntityAlreadyExistsRestaurantException(String message) {
+        super(message);
+    }
+}
