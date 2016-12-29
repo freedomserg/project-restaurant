1bb70904379b31f712b9a22a7ee7806fc494c018
freedomserg <freedomserg97@gmail.com>
2016-12-30 01:01:01 +0200

    user: added Spring security support, login page

--- restaurant-user/src/main/java/net/freedomserg/restaurant/userLayer/web/MainTemplateController.java
+++ restaurant-user/src/main/java/net/freedomserg/restaurant/userLayer/web/MainTemplateController.java
@@ -1,24 +1,84 @@
 package net.freedomserg.restaurant.userLayer.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class MainTemplateController {
 
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView mainPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("user", getPrincipal());
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+
     @RequestMapping(value = "/staff", method = RequestMethod.GET)
-    public String getStaff() {
-        return "staff";
+    public ModelAndView staffPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("user", getPrincipal());
+        modelAndView.setViewName("staff");
+        return modelAndView;
     }
 
     @RequestMapping(value = "/schema", method = RequestMethod.GET)
-    public String getSchema() {
-        return "schema";
+    public ModelAndView schemaPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("user", getPrincipal());
+        modelAndView.setViewName("schema");
+        return modelAndView;
     }
 
     @RequestMapping(value = "/contacts", method = RequestMethod.GET)
-    public String getContacts() {
-        return "contacts";
+    public ModelAndView contactsPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("user", getPrincipal());
+        modelAndView.setViewName("contacts");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/Access_Denied", method = RequestMethod.GET)
+    public ModelAndView accessDeniedPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("user", getPrincipal());
+        modelAndView.setViewName("accessDenied");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView loginPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
+    }
+
+    private String getPrincipal(){
+        String userName = null;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            userName = ((UserDetails)principal).getUsername();
+        } else {
+            userName = principal.toString();
+        }
+        return userName;
     }
 }
---
+++ restaurant-user/src/main/webapp/WEB-INF/views/freemarker/login.ftl
@@ -0,0 +1,56 @@
+<#import "/spring.ftl" as spring />
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Login page</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="<@spring.url '/resources/css/login.css'/>">
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.css" />
+</head>
+
+<body>
+<div id="mainWrapper">
+    <div class="login-container">
+        <div class="login-card">
+            <div class="login-form">
+                <form action="<@spring.url '/login'/>" method="post" class="form-horizontal">
+
+                    <#if (param.error)??>
+                        <div class="alert alert-danger">
+                            <p>Invalid username and password.</p>
+                        </div>
+                    </#if>
+
+                    <#if (param.logout)??>
+                        <div class="alert alert-success">
+                            <p>You have been logged out successfully.</p>
+                        </div>
+                    </#if>
+
+                    <#--<#if Session.SPRING_SECURITY_LAST_EXCEPTION?? && Session.SPRING_SECURITY_LAST_EXCEPTION.message?has_content>-->
+                        <#--<@spring.message "login.bad.credentials"/>-->
+                    <#--</#if>-->
+
+                    <div class="input-group input-sm">
+                        <label class="input-group-addon" for="username"><i class="fa fa-user"></i></label>
+                        <input type="text" class="form-control" id="username" name="ssoId" placeholder="Enter Username" required>
+                    </div>
+                    <div class="input-group input-sm">
+                        <label class="input-group-addon" for="password"><i class="fa fa-lock"></i></label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Enter Password" required>
+                    </div>
+                    <input type="hidden" name="${_csrf.parameterName}"  value="${_csrf.token}" />
+
+                    <div class="form-actions">
+                        <input type="submit"
+                               class="btn btn-block btn-primary btn-default" value="Log in">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
